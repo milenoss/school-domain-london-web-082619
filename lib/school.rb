fca3ae82bf932495dce50a88690a74298bd29ee1
Milen Khanal <milenkhanal@gmail.com>
2019-09-14 19:10:16 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,11 +9,11 @@ def initialize(name)
       @roster["grade"] = []
     end
     
-  describe "#add_student" do
-    it 'is able to add a student' do
-      @school.add_student("AC Slater", 10)
-      expect(@school.roster).to eq({10 => ["AC Slater"]})
-    end
+  # describe "#add_student" do
+  #   it 'is able to add a student' do
+  #     @school.add_student("AC Slater", 10)
+  #     expect(@school.roster).to eq({10 => ["AC Slater"]})
+  #   end
     
     def add_student(student, grade)
       
