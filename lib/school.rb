b8e95d0207ed1ef9acb14170849907ab120ec427
Milen Khanal <milenkhanal@gmail.com>
2019-09-14 18:44:24 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,89 +1,89 @@
 <<<<<<< HEAD
 
-# # code here!# code here!
- require 'pry'
+# # # code here!# code here!
+# require 'pry'
 
- class School
-  def initialize(school_name)
-    @school_name = school_name
-    @roster = {}
-  end
+# class School
+#   def initialize(school_name)
+#     @school_name = school_name
+#     @roster = {}
+#   end
  
-  def roster
-    @roster
-  end
+#   def roster
+#     @roster
+#   end
  
- def add_student(student_name, grade)
-  if @roster[grade] != nil
-    @roster[grade] << student_name
-  else
-  @roster[grade] = [student_name]
- end
- end
+# def add_student(student_name, grade)
+#   if @roster[grade] != nil
+#     @roster[grade] << student_name
+#   else
+#   @roster[grade] = [student_name]
+# end
+# end
  
- def grade(grade)
-  @roster[grade]
- end
+# def grade(grade)
+#   @roster[grade]
+# end
  
- def sort
-  @roster.each{|grade, student|
-  @roster[grade] = student.sort
-  }
- end
+# def sort
+#   @roster.each{|grade, student|
+#   @roster[grade] = student.sort
+#   }
+# end
  
  
-end
-=======
-=======
->>>>>>> b8732ebe40ef00358bee11490ddd744787d70c77
-# code here!
-require 'pry'
+# end
+# =======
+# =======
+# >>>>>>> b8732ebe40ef00358bee11490ddd744787d70c77
+# # code here!
+# require 'pry'
 
-class School
-<<<<<<< HEAD
- def initialize(school_name)
-   @school_name = school_name
-   @roster = {}
- end
+# class School
+# <<<<<<< HEAD
+# def initialize(school_name)
+#   @school_name = school_name
+#   @roster = {}
+# end
 
- def roster
-   @roster
- end
+# def roster
+#   @roster
+# end
 
-def add_student(student_name, grade)
- if @roster[grade] != nil
-   @roster[grade] << student_name
- else
- @roster[grade] = [student_name]
-end
-end
+# def add_student(student_name, grade)
+# if @roster[grade] != nil
+#   @roster[grade] << student_name
+# else
+# @roster[grade] = [student_name]
+# end
+# end
 
-def grade(grade)
- @roster[grade]
-end
+# def grade(grade)
+# @roster[grade]
+# end
 
-def sort
- @roster.each{|grade, student|
- @roster[grade] = student.sort
- }
-end
+# def sort
+# @roster.each{|grade, student|
+# @roster[grade] = student.sort
+# }
+# end
 
 
 
-end
+# end
   
 
-=======
+# =======
   
-  attr_accessor :name, :roster
+#   attr_accessor :name, :roster
 
-  def initialize(name)
-    @name = name
-    @roster = {}
-  end
+#   def initialize(name)
+#     @name = name
+#     @roster = {}
+#   end
   
   
 
 
-end
->>>>>>> b8732ebe40ef00358bee11490ddd744787d70c77
+# end
+# >>>>>>> b8732ebe40ef00358bee11490ddd744787d70c77
